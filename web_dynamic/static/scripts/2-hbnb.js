befd52998580e43b3da737aa1d278a94fa346636
stephen_adah <mailstephenreal@gmail.com>
2024-02-09 13:30:22 +0100

    1-hbnb.js  2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  // Initialize an empty array to store Amenity IDs
+  let amenityIds = [];
+
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function () {
+    // Retrieve Amenity ID from data-id attribute
+    const amenityId = $(this).data('id');
+
+    if ($(this).prop('checked')) {
+      amenityIds.push(amenityId);
+    } else {
+      // If unchecked, remove the Amenity ID from the array
+      amenityIds = amenityIds.filter(id => id !== amenityId);
+    }
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    updateAmenitiesDisplay();
+  });
+
+  // Function to update the h4 tag with the list of Amenities checked
+  function updateAmenitiesDisplay () {
+    const amenitiesText = amenityIds.length > 0 ? amenityIds.join(', ') : '&nbsp;';
+    $('.popover h4').html(amenitiesText);
+  }
+});
+
+$.ajax({
+  url: 'http://0.0.0.0:5001/api/v1/status/',
+  type: 'GET',
+  dataType: 'json',
+  success: function (json) {
+    if (json.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  },
+  error: function (xhr, status) {
+    console.log('error ' + status);
+  }
+});
