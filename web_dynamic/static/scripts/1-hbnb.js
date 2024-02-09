86debcbaa2bcd536732cf26fe7e9bef3e465ba82
stephen_adah <mailstephenreal@gmail.com>
2024-02-09 12:20:22 +0100

    corrected syntax errors and ensure semistandard compliance

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,31 +1,25 @@
-document.ready(function(){
+$(document).ready(function () {
   // Initialize an empty array to store Amenity IDs
-  amenityIds = []
+  let amenityIds = [];
 
   // Listen for changes on each input checkbox tag
-  $('input["type=checkbox"]').change(function() {
+  $('input[type="checkbox"]').change(function () {
     // Retrieve Amenity ID from data-id attribute
     const amenityId = $(this).data('id');
 
-    // Retrieve Amenity name from data-name attribute
-    const amenityName = $(this).data('name');
-
     if ($(this).prop('checked')) {
-      amenityIds.push(amenityId)
+      amenityIds.push(amenityId);
     } else {
       // If unchecked, remove the Amenity ID from the array
-      amenityIds = amenityIds.filter(function(id) {
-        id !== amenityId
-      });
-
+      amenityIds = amenityIds.filter(id => id !== amenityId);
+    }
     // Update the h4 tag inside the div Amenities with the list of Amenities checked
     updateAmenitiesDisplay();
   });
 
   // Function to update the h4 tag with the list of Amenities checked
-  function updateAmenitiesDisplay() {
+  function updateAmenitiesDisplay () {
     const amenitiesText = amenityIds.length > 0 ? amenityIds.join(', ') : '&nbsp;';
     $('.popover h4').html(amenitiesText);
   }
-
 });
